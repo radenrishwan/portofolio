bdd90bcdd7840d8b814e77395b4a1f41cc6a4d55
radenrishwan <radenrishwan@gmail.com>
2024-11-21 19:20:01 +0700

    add timeline

--- components/index/Timeline.vue
+++ components/index/Timeline.vue
@@ -1,3 +1,57 @@
+<script setup lang="ts">
+const timelines = [
+  {
+    title: "First time writing program",
+    description: "writing an Hello World using python",
+    date: "2019-06-23",
+  },
+  {
+    title: "Making a chat bot on line",
+    description:
+      "learning how to code using python, consuming an api, making rest api, and deploying it on heroku",
+    date: "2019-??-??",
+  },
+  {
+    title: "Learning HTML, CSS, and JS",
+    description:
+      "with a big gap hiatus, i'm start learning web development(also waiting for my fasting month)",
+    date: "2020-04-23",
+  },
+  {
+    title: "Making a portofolio website",
+    description: "my first website project",
+    date: "2020-05-??",
+  },
+  {
+    title: "Learn C/C++",
+    description: "Start learning another language",
+    date: "2021-??-??",
+  },
+  {
+    title: "Learning Java",
+    description:
+      "Learning most pain ever language for me (learn on low spec laptop)",
+    date: "2021-??-??",
+  },
+  {
+    title: "Writing Backend Related using Spring ",
+    description: "Learning how to write backend using spring",
+    date: "2021-??-??",
+  },
+  {
+    title: "Learning another backend language (PHP and NodeJS)",
+    description:
+      "Learning another backend language and made some backend project",
+    date: "2021-??-??",
+  },
+  {
+    title: "Learning Golang",
+    description: "Start learning and writing first code golang on my birthday",
+    date: "2022-01-15",
+  },
+];
+</script>
+
 <template>
   <section id="timeline">
     <h1>Cooming Soon</h1>
