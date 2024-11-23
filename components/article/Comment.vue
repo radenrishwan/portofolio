56b3ad5fa5d4858020c89eaa4c04bd179dd763bf
radenrishwan <radenrishwan@gmail.com>
2024-11-23 19:12:44 +0700

    load comments with corrent theme

--- components/article/Comment.vue
+++ components/article/Comment.vue
@@ -1,20 +1,46 @@
-<script setup>
-onMounted(() => {
-  const title = document.querySelector(".article-title").textContent;
+<script setup lang="ts">
+type Theme = "github-light" | "github-dark";
+
+const currentTheme = ref<Theme>("github-light");
+
+const buildComments = (title: string, theme: Theme) => {
+  const utterancesContainer = document.querySelector(".comments-section");
+
+  // check if utterances script already exist
+  if (document.querySelector(".utterances")) {
+    // remove existing script
+    utterancesContainer?.removeChild(
+      document.querySelector(".utterances") as Node,
+    );
+  }
 
   const utterancesScript = document.createElement("script");
   utterancesScript.src = "https://utteranc.es/client.js";
   utterancesScript.setAttribute("repo", "radenrishwan/portofolio");
-  utterancesScript.setAttribute("issue-term", title || "General");
-  utterancesScript.setAttribute("theme", "github-light");
+  utterancesScript.setAttribute("issue-term", title);
+  utterancesScript.setAttribute("theme", theme);
   utterancesScript.setAttribute("crossorigin", "anonymous");
   utterancesScript.async = true;
 
-  const utterancesContainer = document.querySelector(".comments-section");
   if (utterancesContainer) {
     utterancesContainer.appendChild(utterancesScript);
   }
+};
+
+onMounted(() => {
+  // get current theme form root class
+  if (document.documentElement.classList.contains("light-mode")) {
+    currentTheme.value = "github-light";
+  } else {
+    currentTheme.value = "github-dark";
+  }
+
+  const title = document.querySelector(".article-title")?.textContent;
+
+  buildComments(title || "general", currentTheme.value);
 });
+
+// TODO: sync with theme toggle
 </script>
 
 <template>
